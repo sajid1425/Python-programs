4858d9d2599aaa9303ee68671e2c9887956db09a
alfajmahabri <mahabrialfaj@gmail.com>
2024-10-28 15:25:34 +0530

    Data Frame Complete

---
+++ Pandas/DataFrames.py
@@ -0,0 +1,23 @@
+import pandas as pd
+#DataFrame
+a=[10,20,30,40]
+a1=pd.DataFrame(a)
+print(a1)
+
+#labelled column
+data={"Calories":[420,380,120],"Duration":[50,45,45]}
+myvar=pd.DataFrame(data)
+print(myvar)
+
+#using csv
+df = pd.read_csv("C:/Users/mahab/Downloads/Python/Pandas/customers.csv")
+s=df[["First Name", "Last Name", "City"]]
+print(s)
+
+#Head Function
+print("Head Function")
+print(s.head(10))
+
+#Tail Function
+print("Tail Function")
+print(s.head(2))  
--- Pandas/m.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-data={'Name':['Atharv','Varad','Sourabh','Sajid','Sarvesh'],'Age':[24,27,22,32,29]}
-df=pd.DataFrame(data)
-print(df)
-print(df['Age'])
