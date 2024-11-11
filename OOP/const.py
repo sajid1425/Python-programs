abec8539ad7a850f874dffa1597bc740ad78219d
sajid1425 <118750473+sajid1425@users.noreply.github.com>
2024-11-11 15:06:28 +0530

    Delete OOP directory

--- OOP/const.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
-        print(f"Constructor: {self.name} is created.")
-    def __del__(self):
-        print(f"Destructor: {self.name} is deleted.")
-p1 = Person("Alice")
-del p1
